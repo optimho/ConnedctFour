072316e921403d99f9c91c3095d7cbcde911c352
optimho <michael@duplessis.nz>
2022-03-20 15:26:32 +1300

    The first commit

--- myScript.js
+++ myScript.js
@@ -1,21 +1,48 @@
+
 $(document).ready(function() {
-    $("button").click(function () {
-        $("p").hide();
-        console.log("hello click");
-    });
+    
+    var player1 = prompt("Player One, Enter your Name you will be blue")
+    var player1Color = 'rgb(86,151,255)'
+
+    var player2 = prompt("Player 2 enter your name, you are red")
+    var player2Color = 'rgb(237, 45, 73)'
+
+    var gameOn = true;
+    var table = $('table tr')
+
+function reportWin(rowNumber, colNumber) {
+    console.log('you won starting at this row and column');
+    console.log(rowNumber);
+    console.log(colNumber);
+
+}
 
 
-    $("li").click( function(){
-        $(this).addClass("turnBlue");
-    });
 
-    $("li").on('dblclick',  function(){
-        $(this).removeClass("turnBlue");
-    })
+function changeColor(rowIndex,colIndex,color){
 
-    $('input').eq(0).keypress(function (event){
-        console.log(event);
-    });
+    return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color)
+
+}
+
+
+function returnColor(rowIndex,colIndex,color){
+
+        return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color')
+
+    }
+
+
+    function emptyRow(colIndex){
+        var colorReport = returnColor(colIndex,5)
+
+        for(var row = 5; row > -1; row--){
+            colorReport = returnColor(colIndex, row);
+            if (colorReport === 'rgb(125,125,125)'){
+                return row
+            }
+        }
+    }
 
 });
 
